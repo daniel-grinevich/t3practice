073807a8ceb64f0381fb9988dcb21ba3e97d841d
daniel-grinevich <kgbdannn@gmail.com>
2024-07-17 20:36:43 -0400

    vercel update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,14 @@
 import Link from "next/link";
 
+const mockImages = [
+  "https://utfs.io/f/b116a2d2-a43b-4f08-a0b7-63c3f4186875-cf5xva.png",
+  "https://utfs.io/f/1d0695ff-195e-4646-9eed-91732c14ab8e-k40dag.png",
+]
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+    <main className="">
+     <h1>hi</h1>
     </main>
   );
 }
